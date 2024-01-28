45276a937308699fd668772facd275d94ecc1204
brian-ikiara <brnikiara@gmail.com>
2024-01-28 03:53:13 +0300

    :sparkles: Created the first files on repo!
    The files included serve as a sort of skeleton of the full TUI
    application. I hope to add on more info to this.

--- src/client.rs
+++ src/client.rs
@@ -1,8 +1,11 @@
-use std::io::{Write, stdout};
+use std::io::stdout;
 use crossterm::{QueueableCommand, cursor};
+use std::time::Duration;
+use std::thread;
 
 fn main() {
     println!("Hello from Client!");
     let mut stdout = stdout();
     stdout.queue(cursor::MoveTo(5,5)).unwrap();
+    thread::sleep(Duration::from_secs(5));
 }
